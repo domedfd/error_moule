5b97a56a647b45487a0da001a49866349018ed54
Domenico <domedfd@gmail.com>
2018-07-24 00:14:17 +0000

    error

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{ 
+    'name': 'To-Do Application', 
+    'description': 'Manage personal to-do tasks.', 
+    'author': 'Domenico Delvalle', 
+    'depends': ['base'], 
+    'application': True, 
+    'version': '11.0.1.0',
+    'website': 'www.sgsistema.com',
+    'data': [
+	'views/todo_menu.xml',
+	'views/todo_view.xml',
+],
+}
